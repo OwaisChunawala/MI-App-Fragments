3956360708f5683eee1eecc383cd50f483d66ce9
Owais Chunawala <chunawalaowais@gmail.com>
2017-07-08 16:12:20 +0530

    Fragments, tabs, navigation bar done, just the hamburger icon missing from the tool bar; will be added later

--- app/src/main/java/com/example/android/moodindigofragmenttab/MainActivity.java
+++ app/src/main/java/com/example/android/moodindigofragmenttab/MainActivity.java
@@ -1,18 +1,42 @@
 package com.example.android.moodindigofragmenttab;
 
 
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvDrawer;
+    private ActionBarDrawerToggle mDrawerToggle;
+    private CharSequence mTitle;
+    private CharSequence mDrawerTitle;
+
+    private ActionBarDrawerToggle drawerToggle;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+
 
         //calling Main Fragment
         FragmentManager fragmentManager = getSupportFragmentManager();
@@ -21,5 +45,24 @@ public class MainActivity extends AppCompatActivity {
         MainFragment fragment = new MainFragment();
         fragmentTransaction.add(R.id.relative_layout_for_main_fragment, fragment);
         fragmentTransaction.commit();
+
+
+
+
+
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawer.openDrawer(GravityCompat.START);
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
